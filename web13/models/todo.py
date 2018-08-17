7379ae70cc0c5649ae64beeed2ad89bd63767e9e
wangzhen <wang765256860@gmail.com>
2018-08-17 19:34:35 +0800

    没什么新内容

--- web13/models/todo.py
+++ web13/models/todo.py
@@ -1,17 +1,8 @@
 import time
 from models import Model
-
-
 # 针对我们的数据 TODO
 # 我们要做 4 件事情
-"""
-C create 创建数据
-R read 读取数据
-U update 更新数据
-D delete 删除数据
 
-Todo.new() 来创建一个 todo
-"""
 
 class Todo(Model):
     @classmethod
